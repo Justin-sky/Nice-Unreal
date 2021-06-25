dbe2de3a0ad529573a2e7783471d56923d3ab12a
Justin-sky <284139554@qq.com>
2021-06-25 17:51:01 +0800

    init

---
+++ Source/NiceUnreal/NiceUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NiceUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NICEUNREAL_API ANiceUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
